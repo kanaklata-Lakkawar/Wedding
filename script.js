3885a45feead4c620ee703e7145d06503ef9b760
Kanaklata Lakkawar <kanaklataslakkawar@gmail.com>
2023-08-20 10:11:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,36 @@ var x = setInterval(function() {
     document.getElementById("abbreviations").innerHTML = "";
   }
 }, 1000);
+
+const total = 30;
+const dotContainer = document.querySelector(".dot-container");
+const w = window.innerWidth;
+const h = window.innerHeight;
+
+for (let i = 0; i < total; i++) {
+  const dot = document.createElement('div');
+  dot.className = 'dot';
+  dot.style.left = Math.random() * w + 'px';
+  dot.style.top = -Math.random() * h + 'px';
+  dotContainer.appendChild(dot);
+
+  animateDot(dot);
+}
+
+function animateDot(dot) {
+  const duration = Math.random() * 10 + 6; // Random duration between 6 and 16 seconds
+
+  dot.style.transition = `transform ${duration}s linear, opacity ${duration}s linear`;
+  dot.style.transform = `translateY(${h}px)`;
+  dot.style.opacity = '0';
+
+  setTimeout(() => {
+    dot.style.transition = 'none';
+    dot.style.transform = 'translateY(-100px)';
+    dot.style.opacity = '1';
+
+    requestAnimationFrame(() => {
+      animateDot(dot);
+    });
+  }, duration * 1000);
+}
